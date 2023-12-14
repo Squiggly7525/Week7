bb3d8f5cb793fc26be7c4a3eab603d3560d852c3
hastook <147938852+Squiggly7525@users.noreply.github.com>
2023-12-14 18:23:08 +0330

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInputElement.value;
+
+  let apiKey = "bd79ao40tde3dec118ca46bc3e6dd55f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInputElement.value}&key=${apiKey}&unit=metric`;
+
+  axios
+    .get(apiUrl)
+    .then(function (response) {
+      console.log(response);
+      let temperature = Math.round(response.data.temperature.current);
+      let temperatureElement = document.querySelector("#temperature");
+      temperatureElement.innerHTML = `${temperature}`;
+    })
+
+    .catch(function (error) {
+      console.error("Error fetching weather data", error);
+    });
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
